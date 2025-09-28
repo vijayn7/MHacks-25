1152d277e32e31a14fbaf1e006f136dff86c5863
ashmit1002 <ashmits1002@gmail.com>
2025-09-28 01:38:28 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
+"use client"
+
+
 import { useState } from "react"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
 import { motion, AnimatePresence } from "framer-motion"
 import axios from "axios"
 import ScanDashboard from "./components/ScanDashboard"
@@ -7,6 +10,7 @@ import ScanResults from "./components/ScanResults"
 import BuildScreen from "./components/BuildScreen"
 import ConsentModal from "./components/ConsentModal"
 import NavigationBar from "./components/NavigationBar"
+import { Shield } from "lucide-react"
 import "./index.css"
 
 
